0d83ad943e47f5ddfffadea0e94ace2e010614b5
somrads <somradsharma@yahoo.com>
2024-04-26 16:18:30 +0200

    feat: Added server

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send({message: "Hello World"})
+})
+
+app.listen(3000, (err) => {
+    if(!err){
+        console.log("running on port " + 3000);
+    } else {
+        console.error(err)
+    }
+})
+
