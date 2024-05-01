c5c9f6994009ed41df80622851e60fe530f56d7b
somrads <somradsharma@yahoo.com>
2024-05-01 15:37:34 +0200

    feat: Added filter logic to financial data

--- images/frontend/src/App.js
+++ images/frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import FinancialData from './components/FinancialData'
-import BookkeepingData from "./components/BookeepingData"
-import FilterComponent from './components/FilterComponent'
+// import BookkeepingData from "./components/BookeepingData"
+// import FilterComponent from './components/FilterComponent'
+
 
 function App() {
   return (
     <div>
-{/* 
 <FinancialData/>
-<BookkeepingData/> */}
-<FilterComponent/>
+
+
+
     </div>
   )
 }
--- images/frontend/src/components/FinancialData.jsx
+++ images/frontend/src/components/FinancialData.jsx
@@ -2,8 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 const FinancialData = () => {
   const [financialData, setFinancialData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
   const [columnComments, setColumnComments] = useState({});
   const [loading, setLoading] = useState(true);
+  const [filters, setFilters] = useState({
+    bukrs: '',
+    belnr: '',
+    gjahr: '',
+    blart: '',
+    bldat: '',
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -38,30 +46,94 @@ const FinancialData = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    // Apply filters to financial data
+    setFilteredData(
+      financialData.filter(row => {
+        return (
+          row.bukrs.includes(filters.bukrs) &&
+          row.belnr.includes(filters.belnr) &&
+          row.gjahr.includes(filters.gjahr) &&
+          row.blart.includes(filters.blart) &&
+          row.bldat.includes(filters.bldat) 
+
+        );
+      })
+    );
+  }, [financialData, filters]);
+
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilters(prevFilters => ({
+      ...prevFilters,
+      [name]: value
+    }));
+  };
+
   return (
     <div>
       <h2>Financial Data</h2>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <table>
-          <thead>
-            <tr>
-              {Object.keys(columnComments).map((column, index) => (
-                <th key={index}>{columnComments[column]}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {financialData.map((row, index) => (
-              <tr key={index}>
-                {Object.keys(row).map((column, index) => (
-                  <td key={index}>{row[column]}</td>
+        <>
+          <div>
+            <input
+              type="text"
+              name="bukrs"
+              value={filters.bukrs}
+              placeholder="BUKRS"
+              onChange={handleFilterChange}
+            />
+            <input
+              type="text"
+              name="belnr"
+              value={filters.belnr}
+              placeholder="BELNR"
+              onChange={handleFilterChange}
+            />
+            <input
+              type="text"
+              name="gjahr"
+              value={filters.gjahr}
+              placeholder="GJAHR"
+              onChange={handleFilterChange}
+            />
+
+              <input
+              type="text"
+              name="blart"
+              value={filters.blart}
+              placeholder="BLART"
+              onChange={handleFilterChange}
+            />
+              <input
+              type="text"
+              name="bldat"
+              value={filters.bldat}
+              placeholder="BLDAT"
+              onChange={handleFilterChange}
+            />
+          </div>
+          <table>
+            <thead>
+              <tr>
+                {Object.keys(columnComments).map((column, index) => (
+                  <th key={index}>{columnComments[column]}</th>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {filteredData.map((row, index) => (
+                <tr key={index}>
+                  {Object.keys(row).map((column, index) => (
+                    <td key={index}>{row[column]}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       )}
     </div>
   );
